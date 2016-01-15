41c8abd23645a637ff9ed939f47bb4d415ca380e
Frédéric MOHIER <frederic.mohier@gmail.com>
2016-01-15 16:10:39 +0000

    Update mail notification script

--- alignak_notifications/plugins/notify_by_email.py
+++ alignak_notifications/plugins/notify_by_email.py
@@ -54,29 +54,6 @@ def setup_logging():
     else:
         logging.basicConfig(level=log_level, format='%(asctime)s:%(levelname)s: %(message)s')
 
-def overload_test_variable():
-    notification_object_var = {
-        'service': {
-            'Service description': 'CPU load',
-            'Service state': 'PROBLEM',
-            'Service output': 'Houston, we got a problem here! Oh, wait. No. It\'s just a test.',
-            'Service duration': '00h 00min 10s'
-        },
-        'host': {
-            'Hostname': 'Alignak monitoring solution: Test host',
-            'Host state': 'TEST PROBLEM',
-            'Host duration': '00h 00h 20s'
-        }
-    }
-
-    host_service_var = {
-        'Hostname': 'alignak.domain.tld',
-        'Host address': '127.0.0.1',
-        'Notification type': 'TEST',
-        'Date': 'Now, test'
-    }
-    return (notification_object_var, host_service_var)
-
 def get_webui_logo():
     company_logo=''
 
@@ -139,6 +116,9 @@ def get_webui_url():
     if opts.webui:
         hostname = socket.gethostname()
         webui_port = get_webui_port()
+        if not webui_port:
+            return
+
         if opts.webui_url:
             url = '%s/%s/%s' % (opts.webui_url, opts.notification_object, urllib.quote(host_service_var['Hostname']))
         else:
@@ -166,7 +146,19 @@ mail_format = { 'html': MIMEMultipart(), 'txt': MIMEMultipart('alternative') }
 
 # Construct mail subject field based on which object we notify
 def get_mail_subject(object):
-    mail_subject = { 'host': 'Host %s alert for %s since %s' % (notification_object_var['host']['Host state'], host_service_var['Hostname'], notification_object_var['host']['Host duration']), 'service': '%s on Host: %s about service %s since %s' % (notification_object_var['service']['Service state'], host_service_var['Hostname'], notification_object_var['service']['Service description'], notification_object_var['service']['Service duration'])}
+    mail_subject = {
+        'host': 'Host %s alert for %s since %s' % (
+            notification_object_var['host']['Host state'],
+            host_service_var['Hostname'],
+            notification_object_var['host']['Host state duration']
+        ),
+        'service': '%s on Host: %s about service %s since %s' % (
+            notification_object_var['service']['Service state'],
+            host_service_var['Hostname'],
+            notification_object_var['service']['Service description'],
+            notification_object_var['service']['Service state duration']
+        )
+    }
 
     return mail_subject[object]
 
@@ -193,8 +185,8 @@ def create_mail(format):
     msg['From'] = get_user()
     logging.debug('To: %s' % (opts.receivers))
     msg['To'] = opts.receivers
-    logging.debug('Subject: %s' % (get_mail_subject(opts.notification_object)))
-    msg['Subject'] = get_mail_subject(opts.notification_object)
+    logging.debug('Subject: %s' % (opts.prefix + get_mail_subject(opts.notification_object)))
+    msg['Subject'] = opts.prefix + get_mail_subject(opts.notification_object)
 
     return msg
 
@@ -211,6 +203,7 @@ def create_txt_message(msg):
 
     # Add url at the end
     url = get_webui_url()
+    logging.debug('Grabbed WebUI URL : %s' % url)
     if url != None:
         txt_content.append('More details on : %s' % url)
 
@@ -239,23 +232,27 @@ def add_image2mail(img, mail):
     return mail
 
 def create_html_message(msg):
-    # Get url and add it in footer
-    url = get_webui_url()
-    logging.debug('Grabbed WebUI URL : %s' % url)
-
     # Header part
-    html_content = ['''<html><head>\r
-        <meta http-equiv="Content-Type" content="text/html; charset=utf-8"><style type="text/css">body {text-align: center; font-family: Verdana, sans-serif; font-size: 10pt;}\r
-        img.logo {float: left; margin: 10px 10px 10px; vertical-align: middle}\r
-        span {font-family: Verdana, sans-serif; font-size: 12pt;}\r
-        table {text-align:center; margin-left: auto; margin-right: auto;}\r
-        th {white-space: nowrap;}\r
-        th.even {background-color: #D9D9D9;}\r
-        td.even {background-color: #F2F2F2;}\r
-        th.odd {background-color: #F2F2F2;}\r
-        td.odd {background-color: #FFFFFF;}\r
-        th,td {font-family: Verdana, sans-serif; font-size: 10pt; text-align:left;}\r
-        th.customer {width: 600px; background-color: #004488; color: #ffffff;}</style></head><body>\r''']
+    html_content = ['''
+<html>\r
+<head>\r
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">\r
+<style type="text/css">\r
+body {text-align: center; font-family: Verdana, sans-serif; font-size: 10pt;}\r
+img.logo {float: left; margin: 10px 10px 10px; vertical-align: middle}\r
+span {font-family: Verdana, sans-serif; font-size: 12pt;}\r
+table {text-align:center; margin-left: auto; margin-right: auto;}\r
+th {white-space: nowrap;}\r
+th.even {background-color: #D9D9D9;}\r
+td.even {background-color: #F2F2F2;}\r
+th.odd {background-color: #F2F2F2;}\r
+td.odd {background-color: #FFFFFF;}\r
+th,td {font-family: Verdana, sans-serif; font-size: 10pt; text-align:left;}\r
+th.customer {width: 600px; background-color: #004488; color: #ffffff;}\r
+</style>\r
+</head>\r
+<body>\r'''
+    ]
 
     full_logo_path = get_webui_logo()
     if full_logo_path:
@@ -280,10 +277,15 @@ def create_html_message(msg):
             odd=True
 
     html_content.append('</table>')
+
+    # Get url and add it in footer
+    url = get_webui_url()
+    logging.debug('Grabbed WebUI URL : %s' % url)
+
     if url != None:
-        html_content.append('More details on WebUI at : <a href="%s">%s</a></body></html>' % (url, url))
-    else:
-        html_content.append('</body></html>')
+        html_content.append('More details on <a href="%s">WebUI </a>' % (url))
+
+    html_content.append('</body></html>')
 
     # Make final string var to send and encode it to stdout encoding
     # avoiding decoding error.
@@ -305,48 +307,61 @@ def create_html_message(msg):
     return msg
 
 if __name__ == "__main__":
-    parser = OptionParser(description='Send email notifications for Alignak alerts. Message will be formatted in html and can embed a customer logo and a link to the WebUI. To include a customer logo, copy an image file named %s in the directory %s' % (customer_logo, image_dir))
+    parser = OptionParser(description='Send email notifications for Alignak alerts. Message can be formatted in html and embed a customer logo and a link to the WebUI. To include a customer logo, copy an image file named %s in the directory %s' % (customer_logo, image_dir))
 
     group_debug = OptionGroup(parser, 'Debugging and test options', 'Useful to debug the script run by Alignak processes. Useful to just make a standalone test of script to see what it looks like.')
-    group_details = OptionGroup(parser, 'Details and additionnals informations', 'You can include some useful additional information to notifications with these options. Good practice is to add HOST or SERVICE macros with these details and pass them to the script')
-    group_host_service = OptionGroup(parser, 'Host/service macros to specify concerned object.', 'Used to specify usual macros in notify, if not specified then the script will try to get them from environment variable. You need to enable_environment_macros in alignak.cfg if you want to used them. It is not recommanded to use them for large environment. You would better use option -c and -s or -h depend on which object you will notify.')
-    group_general = OptionGroup(parser, 'General options', 'Default options to setup')
+    group_general = OptionGroup(parser, 'General options', 'Default options to setup mail format, and mail information.')
+    group_host_service = OptionGroup(parser, 'Host/service macros to specify concerned object.', 'Used to specify usual macros for notification. If not specified then the script will try to get them from environment variable. You need to enable_environment_macros in alignak.cfg if you want to use them. It is not recommended to use environment variables in large environment. You would better use option -n, -c and -o depending upon which object is concerned.')
+    group_details = OptionGroup(parser, 'Details and additionnals informations', 'You can include some useful additional information to notifications with these options. Good practice is to add HOST or SERVICE macros with these details and provide them to the script')
+    group_webui = OptionGroup(parser, 'Web User Interface.', 'Used to include some Web User Interface information in the notifications.')
 
+    # Debug and test options
     group_debug.add_option('-D', '--debug', dest='debug', default=False,
                       action='store_true', help='Set log level to debug (verbose mode)')
-    group_debug.add_option('-t', '--test', dest='test',
+    group_debug.add_option('-t', '--test', dest='test', default=False,
                       action='store_true', help='Generate a test mail message')
-    group_general.add_option('-w', '--webui', dest='webui', default=False,
-                      action='store_true', help='Include link to the problem in WebUI.')
-    group_general.add_option('-u', '--url', dest='webui_url',
-                      help='WebUI URL as http://my_webui:port/url')
-    group_general.add_option('-f', '--format', dest='format', type='choice', choices=['txt', 'html'],
-                      default='html', help='Mail format "html" or "txt". Default: html')
     group_debug.add_option('-l', '--logfile', dest='logfile',
                       help='Specify a log file. Default: log to stdout.')
+
+    # General options
+    group_general.add_option('-f', '--format', dest='format', type='choice', choices=['txt', 'html'],
+                      default='html', help='Mail format "html" or "txt". Default: html')
+    group_general.add_option('-r', '--receivers', dest='receivers',
+                      help='Mail recipients. At least on enamil address but you can specify a comma-separated list of email addresses.')
+    group_general.add_option('-F', '--sender', dest='sender', default='@'.join((getpass.getuser(), socket.gethostname())),
+                      help='Sender email address, default is system user: %s' % '@'.join((getpass.getuser(), socket.gethostname())))
+    group_general.add_option('-S', '--SMTP', dest='smtp', default='localhost',
+                      help='Target SMTP hostname. None for just a sendmail lanch. Default: localhost')
+    group_general.add_option('-p', '--prefix', dest='prefix', default='',
+                      help='Mail subject prefix. Default is no prefix')
+
+    # Host/service options
+    group_host_service.add_option('-n', '--notification-object', dest='notification_object', type='choice', default='host',
+                      choices=['host', 'service'], help='Choose between host or service notification.')
     group_host_service.add_option('-c', '--commonmacros', dest='commonmacros',
                       help='Double comma separated macros in this order : "NOTIFICATIONTYPE$,,$HOSTNAME$,,$HOSTADDRESS$,,$LONGDATETIME$".')
     group_host_service.add_option('-o', '--objectmacros', dest='objectmacros',
                       help='Double comma separated object macros in this order : "$SERVICEDESC$,,$SERVICESTATE$,,$SERVICEOUTPUT$,,$SERVICEDURATION$" for a service object and "$HOSTSTATE$,,$HOSTDURATION$" with host object')
+
+    # Details options
     group_details.add_option('-d', '--detailleddesc', dest='detailleddesc',
                       help='Specify $_SERVICEDETAILLEDDESC$ custom macros')
     group_details.add_option('-i', '--impact', dest='impact',
                       help='Specify the $_SERVICEIMPACT$ custom macros')
     group_details.add_option('-a', '--action', dest='fixaction',
                       help='Specify the $_SERVICEFIXACTIONS$ custom macros')
-    group_general.add_option('-r', '--receivers', dest='receivers',
-                      help='Mail recipients (To:) comma-separated list')
-    group_general.add_option('-s', '--sender', dest='sender',
-                      help='Mail sender (From:)')
-    group_general.add_option('-n', '--notification-object', dest='notification_object', type='choice', default='host',
-                      choices=['host', 'service'], help='Choose between host or service notification.')
-    group_general.add_option('-S', '--SMTP', dest='smtp', default='localhost',
-                      help='Target SMTP hostname. Default: localhost')
+
+    # WebUI options
+    group_webui.add_option('-w', '--webui', dest='webui', default=False,
+                      action='store_true', help='Include link to the problem in WebUI.')
+    group_webui.add_option('-u', '--url', dest='webui_url',
+                      help='WebUI URL as http://my_webui:port/url')
 
     parser.add_option_group(group_debug)
     parser.add_option_group(group_general)
     parser.add_option_group(group_host_service)
     parser.add_option_group(group_details)
+    parser.add_option_group(group_webui)
 
     (opts, args) = parser.parse_args()
 
@@ -377,11 +392,11 @@ if __name__ == "__main__":
                 'Service description': os.getenv('NAGIOS_SERVICEDESC'),
                 'Service state': os.getenv('NAGIOS_SERVICESTATE'),
                 'Service output': os.getenv('NAGIOS_SERVICEOUTPUT'),
-                'Service duration': os.getenv('NAGIOS_SERVICEDURATION')
+                'Service state duration': os.getenv('NAGIOS_SERVICEDURATION')
             },
             'host': {
                 'Host state': os.getenv('NAGIOS_HOSTSTATE'),
-                'Host duration': os.getenv('NAGIOS_HOSTDURATION')
+                'Host state duration': os.getenv('NAGIOS_HOSTDURATION')
             }
         }
     else:
@@ -392,11 +407,11 @@ if __name__ == "__main__":
                     'Service description': macros[0],
                     'Service state': macros[1],
                     'Service output': macros[2],
-                    'Service duration': macros[3]
+                    'Service state duration': macros[3]
                 },
                 'host': {
                     'Host state': '',
-                    'Host duration': ''
+                    'Host state duration': ''
             }
 
             }
@@ -406,23 +421,45 @@ if __name__ == "__main__":
                     'Service description': '',
                     'Service state': '',
                     'Service output': '',
-                    'Service duration': ''
+                    'Service state duration': ''
                 },'host': {
                     'Host state': macros[0],
-                    'Host duration': macros[1]
+                    'Host state duration': macros[1]
                 }
             }
 
     # Load test values
     if opts.test:
-        notification_object_var, host_service_var = overload_test_variable()
+        notification_object_var = {
+            'service': {
+                'Service description': 'Test_Service',
+                'Service state': 'TEST',
+                'Service output': 'Houston, we got a problem here! Oh, wait. No. It\'s just a test.',
+                'Service state duration': '00h 00min 10s'
+            },
+            'host': {
+                'Hostname': 'Test_Host',
+                'Host state': 'TEST',
+                'Host state duration': '00h 00h 20s'
+            }
+        }
+
+        host_service_var = {
+            'Hostname': 'alignak',
+            'Host address': '127.0.0.1',
+            'Notification type': 'TEST',
+            'Date': 'Now, test'
+        }
+
+    logging.debug('notification_object_var: %s', notification_object_var)
+    logging.debug('host_service_var: %s', host_service_var)
 
     if not host_service_var or not host_service_var['Hostname']:
-        logging.error('You must define at least some host/service information (-c), or test mode (-t)')
+        logging.error('You must define at least some host/service information (-c) or specify test mode (-t)')
         sys.exit(6)
 
     # check required arguments
-    if opts.receivers == None:
+    if not opts.receivers:
         logging.error('You must define at least one mail recipient using -r')
         sys.exit(5)
     else:
@@ -445,11 +482,29 @@ if __name__ == "__main__":
     elif opts.format == 'txt':
         mail = create_txt_message(mail)
 
-    logging.debug('Connecting to %s smtp server' % (opts.smtp))
     try:
         smtp = smtplib.SMTP(opts.smtp)
         logging.debug('Send the mail')
         smtp.sendmail(get_user(), receivers, mail.as_string())
         logging.info("Mail sent successfuly")
+        # Use SMTP or sendmail to send the mail ...
+        if opts.smtp != 'None':
+            logging.debug('Connecting to %s smtp server' % (opts.smtp))
+            smtp = smtplib.SMTP(opts.smtp)
+            logging.debug('Send the mail')
+            smtp.sendmail(opts.smtpfrom, receivers, mail.as_string())
+            logging.info("Mail sent successfuly")
+        else:
+            sendmail = '/usr/sbin/sendmail'
+            logging.debug('Send the mail')
+            p = os.popen('%s -t' % sendmail, 'w')
+            logging.debug('Final mail : ' + mail.as_string())
+            logging.debug('Send the mail')
+            p.write(mail.as_string())
+            status = p.close()
+            if status is not None:
+                logging.error("Sendmail returned %s" % status)
+            else:
+                logging.info("Mail sent successfuly")
     except Exception as e:
         logging.error("Error when sending mail: %s", str(e))
