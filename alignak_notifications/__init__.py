32416949f0876ec0ade5f9bf47ba1384dfd86d67
Frédéric MOHIER <frederic.mohier@gmail.com>
2016-10-19 07:10:50 +0200

    Prepare package setup

--- alignak_notifications/__init__.py
+++ alignak_notifications/__init__.py
@@ -1,23 +1,10 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-# Copyright (c) 2015:
+# Copyright (c) 2015-2016:
 #   Frederic Mohier, frederic.mohier@gmail.com
+#   David Durieux, d.durieux@siprossii.com
 #
-
 """
-    Alignak - Checks pack for monitoring hosts with NRPE
+    Alignak - Notifications pack
 """
-# Checks types for PyPI keywords
-# Used for:
-# - PyPI keywords
-# - directory where to store files in the Alignak configuration (eg. arbiter_cfg/packs/checks_type)
-__checks_type__ = "nrpe"
-
-# Application manifest
-__version__ = u"0.1.0"
-__author__ = u"Frédéric MOHIER"
-__author_email__ = "frederic.mohier@gmail.com"
-__copyright__ = u"(c) 2015 - %s" % __author__
-__license__ = u"GNU Affero General Public License, version 3"
-__url__ = u"https://github.com/Alignak-monitoring-contrib/alignak-notifications"
